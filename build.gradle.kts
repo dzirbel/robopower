8aec19b325a8a34c546a4e4c451504863ebb3863
Dominic Zirbel <dominiczirbel@gmail.com>
2022-12-23 17:16:49 -0800

    Add JaCoCo test coverage configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // TODO jacoco and codecov
 
 plugins {
     kotlin("jvm") version libs.versions.kotlin
     alias(libs.plugins.detekt)
+    jacoco
 }
 
 allprojects {
@@ -14,9 +19,37 @@ allprojects {
 val runtimeAssertions = findProperty("runtime.assertions")?.toString()?.toBoolean() == true
 
 subprojects {
+    configurations.all {
+        resolutionStrategy {
+            failOnNonReproducibleResolution()
+        }
+    }
+
+    // fail compilation tasks for compiler warnings
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.allWarningsAsErrors = true
+    }
+
+    // enable assertions for execution tasks based on the project property
     if (runtimeAssertions) {
-        tasks.withType<JavaExec>().configureEach {
+        tasks.withType<JavaExec> {
             enableAssertions = true
+        }
+    }
+
+    // improve logging of test results
+    tasks.withType<Test> {
+        testLogging {
+            events = setOf(TestLogEvent.FAILED, TestLogEvent.STANDARD_ERROR, TestLogEvent.STANDARD_OUT)
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
+
+    tasks.withType<JacocoReport> {
+        reports {
+            xml.required.set(true)
+            csv.required.set(false)
         }
     }
 
@@ -36,6 +69,10 @@ subprojects {
 
         tasks.test {
             useJUnitPlatform()
+        }
+
+        jacoco {
+            toolVersion = libs.versions.jacoco.get()
         }
     }
 }
--- runner/build.gradle.kts
+++ runner/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version libs.versions.kotlin
     alias(libs.plugins.detekt)
+    jacoco
     application
 }
 
